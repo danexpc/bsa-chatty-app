7c6e7c587d974d0d402e1d288f3e86e4858b54d5
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-09 22:59:03 +0300

    Add bootstrap styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Alert from 'react-bootstrap/Alert';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+        <Alert  variant="danger">
+            This is a alert—check it out!
+        </Alert>
     </div>
   );
 }
