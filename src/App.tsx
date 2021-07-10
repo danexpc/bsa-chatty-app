c04d428aa7d6edaed61358fb76303ef47c69ed15
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-10 10:14:49 +0300

    Create message list template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
+import {MessageList} from "./components/MessageList";
+import {MessageInput} from "./components/MessageInput";
 
 
 function App() {
   return (
     <div className="container">
         <Header />
+        <MessageList />
+        <MessageInput />
     </div>
   );
 }
--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import {Message} from "./Message";
 
 interface MessageListProps {
 }
 
 export const MessageList: React.FC<MessageListProps> = () => {
     return (
-        <>
-        </>
+        <div className="message-list">
+            <span className="messages-divider"/>
+            <Message />
+            <Message />
+            <Message />
+            <span className="messages-divider"/>
+            <Message />
+            <Message />
+            <Message />
+        </div>
     )
 }
