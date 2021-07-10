dabda799e3855d7a5d5fc74942be55499c46d18e
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-10 22:41:50 +0300

    Add service that transforms json to IMessage objecs

---
+++ src/services/transform.service.tsx
@@ -0,0 +1,22 @@
+import {IMessage} from "../interfaces/message";
+
+class TransformService {
+    transform(message: IMessage): IMessage {
+        return {
+            id: message.id,
+            userId: message.userId,
+            avatar: message.avatar,
+            user: message.user,
+            text: message.text,
+            createdAt: new Date(message.createdAt),
+            editedAt: message.editedAt ? new Date(message.editedAt) : undefined
+        }
+
+    }
+
+    transformAll(messages: IMessage[]): IMessage[] {
+        return messages.map(message => this.transform(message))
+    }
+}
+
+export const transformService = new TransformService();
