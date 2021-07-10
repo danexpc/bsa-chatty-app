659fbc394c08f057ada6daf44c95c6d23172fca2
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-10 10:06:55 +0300

    Create header structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import Alert from 'react-bootstrap/Alert';
 import './App.css';
+import {Header} from "./components/Header";
 
 
 function App() {
   return (
-    <div className="App">
-        <Alert  variant="danger">
-            This is a alert—check it out!
-        </Alert>
+    <div className="container">
+        <Header />
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import {Container, Navbar} from "react-bootstrap";
 
 interface HeaderProps {
 }
 
 export const Header: React.FC<HeaderProps> = () => {
     return (
-        <>
-        </>
+            <Navbar className="header mt-5">
+                <Container className="align-items-center">
+                    <Navbar.Brand href="#home" className="mr-5 header-title">My Chat</Navbar.Brand>
+                    <Navbar.Collapse className="justify-content-start">
+                        <Navbar.Text className="mr-5 header-users-count">
+                            23 participants
+                        </Navbar.Text>
+                        <Navbar.Text className="header-messages-count">
+                            53 messages
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                    <Navbar.Text className="justify-content-end header-last-message-date">
+                        Last message at 14.28
+                    </Navbar.Text>
+                </Container>
+            </Navbar>
     );
 }
