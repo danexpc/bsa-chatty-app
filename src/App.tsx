396d9277f47a9f9308e00711085048881105656f
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-10 23:05:50 +0300

    Render header dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,12 @@ class App extends Component<any, any> {
         this.renderMessages();
     }
 
-    renderMessages = async() => {
+    renderMessages = async () => {
         try {
             const messages: IMessage[] = transformService.transformAll(await apiService.fetchMessages());
 
             this.setState({
-                messages: messages
+                messages: messages.sort((message1, message2) => message1.createdAt > message2.createdAt ? 1 : -1)
             });
         } catch (err) {
             console.log(err);
@@ -30,10 +30,17 @@ class App extends Component<any, any> {
 
     render() {
         if (this.state.messages.length !== 0) {
-            console.log(this.state.messages)
+            const participants: string[] = []
+            this.state.messages.forEach(message => {
+                if (!participants.includes(message['user'])) {
+                    participants.push(message['user'])
+                }
+            })
+            const lastMessageDate: Date = this.state.messages[this.state.messages.length - 1]['createdAt'];
             return (
                 <div className="container">
-                    <Header/>
+                    <Header participantsCount={participants.length} messagesCount={this.state.messages.length}
+                            lastMessageDate={`${lastMessageDate.toLocaleDateString()} ${lastMessageDate.getHours()}:${lastMessageDate.getMinutes()}`}/>
                     <MessageList messages={this.state.messages}/>
                     <MessageInput/>
                 </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,23 +2,26 @@ import React from "react";
 import {Container, Navbar} from "react-bootstrap";
 
 interface HeaderProps {
+    participantsCount: number,
+    messagesCount: number,
+    lastMessageDate: string
 }
 
-export const Header: React.FC<HeaderProps> = () => {
+export const Header: React.FC<HeaderProps> = (props) => {
     return (
             <Navbar className="header mt-4">
                 <Container className="align-items-center">
                     <Navbar.Brand href="#home" className="mr-5 header-title">My Chat</Navbar.Brand>
                     <Navbar.Collapse className="justify-content-start">
                         <Navbar.Text className="mr-5 header-users-count">
-                            23 participants
+                            {props.participantsCount} participants
                         </Navbar.Text>
                         <Navbar.Text className="header-messages-count">
-                            53 messages
+                            {props.messagesCount} messages
                         </Navbar.Text>
                     </Navbar.Collapse>
                     <Navbar.Text className="justify-content-end header-last-message-date">
-                        Last message at 14.28
+                        Last message at {props.lastMessageDate}
                     </Navbar.Text>
                 </Container>
             </Navbar>
