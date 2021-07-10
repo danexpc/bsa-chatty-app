cece2e2654ddd50afc7e4c725158f8f7a0b987d0
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-10 17:13:18 +0300

    Create InputMessage component template

--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import {Button, Form} from "react-bootstrap";
 
 interface MessageInputProps {
 }
 
 export const MessageInput: React.FC<MessageInputProps> = () => {
     return (
-        <>
-        </>
+        <div className="message-input">
+            <Form.Control className="message-input-text" type="text" placeholder="Message" size="sm"/>
+            <Button type="submit" className="message-input-button" size="sm">
+                Send
+            </Button>
+        </div>
     )
 }
