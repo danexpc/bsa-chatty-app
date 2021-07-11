94fb25be5e93a909903c8bcdaa47ece7bae84dd9
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-11 14:25:31 +0300

    Add bsa.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Chat from './components/chat/Chat';
+import Chat from './bsa';
 
 ReactDOM.render(
     <React.StrictMode>
