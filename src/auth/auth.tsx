65eb4a7079a1575d1f5648eeaf3ceb88706ce5be
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-11 14:48:22 +0300

    Create getUser() functions that returns current user

---
+++ src/auth/auth.tsx
@@ -0,0 +1,8 @@
+import {IUser} from "../interfaces/user";
+
+export const getUser = (): IUser => {
+    return {
+        id: "6d57a02a-e0f7-4897-bed1-ba2f49796f69",
+        username: "Dan"
+    }
+}
