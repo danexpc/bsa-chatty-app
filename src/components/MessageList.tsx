b6ddf1c25a370107b881ce11c3ac987db99574a1
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-11 11:16:54 +0300

    Rendering messages by number of day

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -11,28 +11,52 @@ interface MessageListProps {
 }
 
 export const MessageList: React.FC<MessageListProps> = (props) => {
+
+    let messagesByDay: Map<string, IMessage[]> = new Map<string, IMessage[]>();
+
+    props.messages.forEach(message => {
+        let day: string  = message.createdAt.getDate().toString();
+        if (messagesByDay.has(day)) {
+            let messages = messagesByDay.get(day);
+            if (messages) {
+                messages.push(message)
+                messagesByDay.set(day, messages)
+                return
+            }
+        }
+        messagesByDay.set(day, Array.of(message))
+    })
+
+    let renderOutput: JSX.Element[] = [];
+
+    messagesByDay.forEach((messages, day) => {
+        renderOutput.push(
+            <div key={day} className="message-divider">{day}</div>
+        )
+        messages.forEach(message => {
+            if (message.userId === "6d57a02a-e0f7-4897-bed1-ba2f49796f69") {
+                renderOutput.push(<OwnMessage key={message.id}
+                                   text={message.text}
+                                   createdAt={message.createdAt.getHours() + ":" + message.createdAt.getMinutes()}
+                                   onDelete={() => props.onDelete(message.id)}
+                                   onEdit={() => props.onEdit(message.id, message.text)}/>)
+            } else {
+                renderOutput.push(<Message key={message.id}
+                                id={message.id} userId={message.userId} avatar={message.avatar}
+                                user={message.user}
+                                text={message.text}
+                                createdAt={message.createdAt.getHours() + ":" + message.createdAt.getMinutes()}
+                                liked={message.liked}
+                                onLike={() => props.onLike(message.id)}/>)
+            }
+        })
+    })
+
+
     return (
         <div className="message-list">
-            <div className="message-divider">Yesterday</div>
             {
-                props.messages.map(message => {
-                    if (message.userId === "6d57a02a-e0f7-4897-bed1-ba2f49796f69") {
-                        return <OwnMessage key={message.id}
-                                        text={message.text}
-                                        createdAt={message.createdAt.getHours() + ":" + message.createdAt.getMinutes()}
-                                        onDelete={() => props.onDelete(message.id)}
-                                        onEdit={() => props.onEdit(message.id, message.text)}/>
-                    } else {
-                        return <Message key={message.id}
-                                        id={message.id} userId={message.userId} avatar={message.avatar} user={message.user}
-                                        text={message.text}
-                                        createdAt={message.createdAt.getHours() + ":" + message.createdAt.getMinutes()}
-                                        liked={message.liked}
-                                        onLike={() => props.onLike(message.id)}/>
-                    }
-
-                    }
-                )
+                renderOutput
             }
         </div>
     )
