f1fa632a0975651c2c7c05708a024efd542645b6
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-11 16:31:10 +0300

    Move import bootstrap css at top

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Chat from './components/chat/Chat';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Chat from './components/chat/Chat';
 import './index.css';
 
 ReactDOM.render(
