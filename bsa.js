7dd1b197f637d2d6ef2df688f25c27206008de26
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-11 15:33:37 +0300

    Move file bsa.js to root

---
+++ bsa.js
@@ -0,0 +1,3 @@
+import Chat from "./src/components/chat/Chat";
+
+export default Chat;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import Chat from './components/chat/Chat';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Chat from './bsa';
+import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
